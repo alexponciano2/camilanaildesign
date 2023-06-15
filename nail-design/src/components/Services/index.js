52667af4e1b2f8444eb5150f3e6c36d322c3483a
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-06-15 16:33:01 -0300

    update

--- nail-design/src/components/Services/index.js
+++ nail-design/src/components/Services/index.js
@@ -7,48 +7,47 @@ function Services() {
                 <h1 className="servicos">NOSSOS <strong>SERVIÇOS</strong></h1>
             </section>
             
-            <section className={styles.services}>
-                <div className={styles.bg1}>
-                    <div className={styles.alongamento}>
-                        <h2 className="servh2">ALONGAMENTO EM GEL</h2>
+            <section className={styles.tudo}>
+                <section className={styles.services}>
+                    <div className={styles.bg1}>
+                        <div className={styles.alongamento}>
+                            <h2 className="servh2">ALONGAMENTO EM GEL</h2>
+                        </div>
+                        <div className={styles.alongamento}>
+                            <h2 className="servh2">BANHO EM GEL</h2>
+                        </div>
                     </div>
-                    <div className={styles.alongamento}>
-                        <h2 className="servh2">BANHO EM GEL</h2>
+                </section>
+                <section className={styles.services}>
+                    <div className={styles.bg2}>
+                        <div className={styles.alongamento}>
+                            <h2 className="servh2">BLINDAGEM MÃOS E PÉS</h2>
+                        </div>
+                        <div className={styles.alongamento}>
+                            <h2 className="servh2">MANUTENÇÃO EM GEL</h2>
+                        </div>
                     </div>
-                </div>
-            </section>
-
-            <section className={styles.services}>
-                <div className={styles.bg2}>
-                    <div className={styles.alongamento}>
-                        <h2 className="servh2">BLINDAGEM MÃOS E PÉS</h2>
+                </section>
+                <section className={styles.services}>
+                    <div className={styles.bg3}>
+                        <div className={styles.alongamento}>
+                            <h2 className="servh2">MANICURE E PEDICURE TRADICIONAL</h2>
+                        </div>
+                        <div className={styles.alongamento}>
+                            <h2 className="servh2">ESMALTAÇÃO EM GEL</h2>
+                        </div>
                     </div>
-                    <div className={styles.alongamento}>
-                        <h2 className="servh2">MANUTENÇÃO EM GEL</h2>
+                </section>
+                <section className={styles.services}>
+                    <div className={styles.bg4}>
+                        <div className={styles.alongamento}>
+                            <h2 className="servh2">CUTILAGEM SEM ESMALTAÇÃO</h2>
+                        </div>
+                        <div className={styles.alongamento}>
+                            <h2 className="servh2">NAIL ART</h2>
+                        </div>
                     </div>
-                </div>                
-            </section>
-
-            <section className={styles.services}>
-                <div className={styles.bg3}>
-                    <div className={styles.alongamento}>
-                        <h2 className="servh2">MANICURE E PEDICURE TRADICIONAL</h2>
-                    </div>
-                    <div className={styles.alongamento}>
-                        <h2 className="servh2">ESMALTAÇÃO EM GEL</h2>
-                    </div>
-                </div>
-            </section>
-
-            <section className={styles.services}>
-                <div className={styles.bg4}>
-                    <div className={styles.alongamento}>
-                        <h2 className="servh2">CUTILAGEM SEM ESMALTAÇÃO</h2>
-                    </div>
-                    <div className={styles.alongamento}>
-                        <h2 className="servh2">NAIL ART</h2>
-                    </div>
-                </div>
+                </section>
             </section>
         </>
 
